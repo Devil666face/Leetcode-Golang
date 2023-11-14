9695e76c2af3b583293ed40821bcf302cf7cbb2e
Devil666face <artem1999k@gmail.com>
2023-11-14 17:19:26 +0300

    1

---
+++ 1.[Easy]Two Sum/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	twoSum([]int{2, 7, 11, 15}, 9)
+}
+
+func twoSum(nums []int, target int) []int {
+	for i, v1 := range nums {
+		for j, v2 := range nums {
+			if i != j && v1+v2 == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+
+}
